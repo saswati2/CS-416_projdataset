36893f5f4499050bcfc90be6d8817247e2ad197c
saswati2 <161897421+saswati2@users.noreply.github.com>
2024-06-15 21:00:42 -0500

    Create merging_dataset.py

---
+++ merging_dataset.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+
+#Put your .csv file location
+directory = '/Users/munssa01/Downloads/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports_us'
+
+dataframes = []
+
+for filename in os.listdir(directory):
+    if filename.endswith('.csv'):
+        file_path = os.path.join(directory, filename)
+        df = pd.read_csv(file_path)
+        # Extract date from filename (filename is something like '2020-01-01.csv')
+        date = os.path.splitext(filename)[0]  # Removes the file extension
+        # Add a new column for the date
+        df['date_1'] = date
+        dataframes.append(df)
+
+merged_df = pd.concat(dataframes, ignore_index=True)
+
+merged_df.to_csv('merged_covid_data.csv', index=False)
